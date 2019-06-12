91d958bb08f94a2072eccad5e55b120141c6fb60
Zenas Muli <zenas2294@gmail.com>
2019-06-12 12:19:33 +0300

    Linked MainActivity with MadlibsActivity

--- app/src/main/java/com/epicodus/madlibs/MainActivity.java
+++ app/src/main/java/com/epicodus/madlibs/MainActivity.java
@@ -2,13 +2,32 @@ package com.epicodus.madlibs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button start;
+    private TextView madLibs;
+    private TextView description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start = (Button) findViewById(R.id.btnStart);
+        madLibs = (TextView) findViewById(R.id.tvMadlibs);
+        description = (TextView) findViewById(R.id.tvDescription);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Madlibs.class);
+                startActivity(intent);
+            }
+        });
     }
 }
